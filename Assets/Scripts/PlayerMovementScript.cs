a0dc6ea75787792941ec7ef59c41235cce015aaf
Sullivan Abegg <sullivanabegg@gmail.com>
2023-05-16 09:40:31 -0400

    Changed Animation state in code corrisponding to movment

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerMovementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float runSpeed = 10f;
+    Vector2 moveInput; 
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Run();
+        FlipSprite();
     }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+    
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+
+        bool playerHashorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHashorizontalSpeed);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHashorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if(playerHashorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+
 }
