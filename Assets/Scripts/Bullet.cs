77db4474e67bc8b269fa61cbc0c904c14e55e4bd
Sullivan Abegg <sullivanabegg@gmail.com>
2023-05-30 09:48:00 -0400

    5/30/23

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,15 +6,19 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] float bulletSpeed = 20f;
     Rigidbody2D myRigidbody;
+    PlayerMovement player;
+    float xSpeed;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        player = FindObjectofType<PlayerMovement>();
+        xSpeed = player.transform.localScale.x * bulletSpeed;
     }
 
 
     void Update()
     {
-        myRigidbody.velocity = new Vector2 (bulletSpeed, 0f);
+        myRigidbody.velocity = new Vector2 (xSpeed, 0f);
     }
 }
--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PlayerMovementScript : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
